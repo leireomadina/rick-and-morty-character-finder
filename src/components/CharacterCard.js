37e11bf46ac25f68cef9ea78203e31f4818c89c1
Leire Ordeñana <leireomadina@gmail.com>
2020-11-23 18:52:49 +0100

    Add key to li element and CharacterList propTypes

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 const CharacterCard = (props) => {
-  console.log(props.character);
+  // console.log(props.character);
   return (
     <div>
       {props.character.name}
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,13 +1,15 @@
 import React from "react"
+import PropTypes from "prop-types";
 import CharacterCard from "./CharacterCard";
 
 const CharacterList = (props) => {
 
 // console.log(props.characters);
 const character = props.characters.map((character) => {
-  console.log(character);
+  // console.log(character);
+  // console.log(character.id);
   return (
-    <li>
+    <li  key={character.id} >
       <CharacterCard character={character}/>
     </li>
   );
@@ -22,5 +24,10 @@ const character = props.characters.map((character) => {
     </section>
   )
 };
+
+CharacterList.propTypes = {
+  character: PropTypes.array,
+  id: PropTypes.number
+}
 
 export default CharacterList;
