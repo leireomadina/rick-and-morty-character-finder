5a20b626a86950ca24d9bc13db5b0282e758970c
Leire Ordeñana <leireomadina@gmail.com>
2020-11-23 17:17:17 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ejercicio de evaluación final con React - Módulo 3 Adalab
+
+Este proyecto ha sido desarrollado por **Leire Ordeñana Madina** como alumna de Adalab.
---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import '../stylesheets/App.scss';
+
+const App = () => {
+  return (
+    <div className="App">
+      Holi :)
+    </div>
+  );
+}
+
+export default App;
