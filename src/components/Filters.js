2e4fb2727f5765376ec56693ba4e153bd01ea48f
Leire Ordeñana <leireomadina@gmail.com>
2021-02-05 10:06:18 +0100

    Add status and gender html form selects

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -17,8 +17,25 @@ const Filters = (props) => {
   return (
     <section className="form-section">
       <form onSubmit={handleSubmit}>
+        {/* Character name */}
         <label htmlFor="character"></label>
         <input type="text" id="character" name="character" placeholder="Search you favorite character" onChange={handleFilter} value={props.filterText} className="form-section__input"></input>
+        {/* Gender */}
+        <label htmlFor="gender"></label>
+        <select id="gender" name="gender">
+          <option value="all">All</option>
+          <option value="female">Female</option>
+          <option value="male">Male</option>
+          <option value="unkown">Other</option>
+        </select>
+        {/* Status */}
+        <label htmlFor="status"></label>
+        <select id="status" name="status">
+          <option value="all">All</option>
+          <option value="alive">Alive</option>
+          <option value="dead">Dead</option>
+          <option value="unkown">Unkown</option>
+        </select>
       </form>
     </section>
   )
