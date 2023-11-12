b8da7bf9fe3159f6beaeb27beb9270a4dc6bbc55
leireomadina <leireomadina@gmail.com>
2023-11-12 13:46:32 +0100

    Update dependencies and adapt tests

--- src/components/CharacterList.test.tsx
+++ src/components/CharacterList.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import 'intersection-observer';
@@ -32,7 +32,6 @@ describe('Character list component', () => {
   it('renders without errors', () => {
     renderCharacterList();
     const ulElement = screen.getByTestId('character-list');
-    console.log(ulElement)
     expect(ulElement).toHaveClass('cards-container');
   });
   
